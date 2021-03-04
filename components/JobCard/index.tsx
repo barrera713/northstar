e70defbf1fc9bce3f754109af936f1261eb18688
carlos barrera <dev.carlos96@gmail.com>
2021-03-04 00:31:49 -0600

    restyled Job card

--- components/JobCard/index.tsx
+++ components/JobCard/index.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { JobCardStyles } from './styles';
-import { Card, Avatar } from 'antd';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card, Avatar, Button } from 'antd';
+import { SaveOutlined } from '@ant-design/icons';
 
 interface JobCardProps {}
 
 export const JobCard: React.FC<JobCardProps> = ({}) => { 
 
     const { Meta } = Card;
+    const [fillHeart, setFillHeart] = useState(false);
 
     return (
         <JobCardStyles>
               <Card
+                hoverable={true}
                 className='card-container'
             >
                 <Meta
                 avatar={<Avatar src="https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBazJiIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--4befa4f6603e069d3d64380dfbf7b42b2af3e4d4/Round_Logo1%20(1).png" />}
-                title="Studio Game Engineer - Asia"
-                description="Remote/Europe - Full Time"
+                title={<p className='meta-title'>Studio Game Engineer - Asia</p>}
+                description={<p className='meta-description'>Remote/Europe - Full Time</p>}
                 />
                 <div className='card-description'>
                 <p>Here at Playco, we make games that bring the world closer together through play.</p>
                 <p>Our ideal teammates are thoughtful, humble, and passionate professionals who can both zoom into the details and zoom out to embrace the big picture. We are inspired by makers and tinkerers, who are unafraid to experiment with a healthy disregard for constraints. We hope you'll enrich our team with your enthusiasm and passion, both for games and for life.</p>
+                </div>
+                <div className='card-footer'>
+                    <Button type='primary' shape='round'>
+                        Apply now
+                    </Button>
+                    <Button 
+                    shape='round'
+                    size='middle' 
+                    className='card-save-button'>
+                        <SaveOutlined /> Save
+                    </Button>
+                    <div className='posted-date'>1d</div>
                 </div>
             </Card>
         </JobCardStyles>
--- components/JobCard/styles.ts
+++ components/JobCard/styles.ts
@@ -5,17 +5,45 @@ export const JobCardStyles = styled.div`
 
     .card-container {
         max-width: 300px;
+        height: 250px;
         border-radius: 10px;
-        margin: 7px -14px;
+        margin: 7px 0;
+        background: #4B4B4B;
+     
+        border: none;
     }
 
+    .meta-title {
+        color: #ffffff;
+    }
+
+    .meta-description {
+        margin-top: -20px;
+        color: #bfbfbf;
+    };
+
     .card-description {
-        margin-top: 10px;
+        color: #ffffff;
     }
 
     .card-description p:nth-child(2) {
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
+    }
+
+    .card-footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .card-save-button {
+        position: relative;
+        right: 3%;
+    }
+
+    .posted-date {
+        color: #bfbfbf;
     }
 `

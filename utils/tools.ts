2315019f5033f418d573eb8a3db9d487d4144ce1
carlos barrera <dev.carlos96@gmail.com>
2021-03-24 14:06:00 -0500

    removed unecessary comments

--- utils/tools.ts
+++ utils/tools.ts
@@ -2,18 +2,11 @@
 
 */
 
-export const datePosted = (postedDate: string) => {
-    // convert input date to new date
-    // create const with current date
-    // subtract current - input
-    // save diff in new date
-    // get hours from diff date
-    
+export const datePosted = (postedDate: string) => {  
     let inputDate = new Date(postedDate);
     let currentDate = new Date();
     
 
     let difference = currentDate.setDate(currentDate.getDate() - inputDate.getDate());
-    // console.log('FUNCTION output => ', new Date(difference).getDate())
     return new Date(difference).getDate();
 };
